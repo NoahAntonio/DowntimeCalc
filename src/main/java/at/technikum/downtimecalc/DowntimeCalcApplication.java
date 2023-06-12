a9adf30566f0a834323e994dc8fd8a6d66c40ab5
Noah Seyhan <wi22b128@technikum-wien.at>
2023-06-12 19:48:40 +0200

    Initial commit

---
+++ src/main/java/at/technikum/downtimecalc/DowntimeCalcApplication.java
@@ -0,0 +1,13 @@
+package at.technikum.downtimecalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DowntimeCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DowntimeCalcApplication.class, args);
+	}
+
+}
